4826f43e1e195d12a0a3b5531967e9edab2f11e7
Jessica Chen <jessicachen@Jessicas-MBP.fios-router.home>
2022-07-08 01:00:59 -0400

    bg works

--- components/Hero.js
+++ components/Hero.js
@@ -40,68 +40,90 @@ const Hero = ({
   ],
 }) => {
   return (
-    // <div className="max-w-screen-xl mt-10 px-8 xl:px-16 mx-auto" id="about">
-    <div className="bg-scroll bg-my_bg_image h-[972px] max-w-screen-xl mt-10 px-8 xl:px-16 mx-auto" id="about">
-        
-      <div className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16 ">
-        <div className="inline-block align-top py-12 flex-col justify-center items-start row-start-2 sm:row-start-1">
-          <h1 className="text-5xl xl:text-6xl font-medium text-black-600 leading-normal">
-            Our future is on the <strong>ballot</strong>.
-          </h1>
-          <p className="text-black-500 mt-4 mb-6">
-            Vote in the Massachusetts primaries on September 6th.
-          </p>
-          {/* <ButtonPrimary>Get Started</ButtonPrimary> */}
-            <Button >Meet the Candidates</Button>
+    <div className='
+      bg-[url("https://img2.storyblok.com//f/64062/1444x543/9f4b93d8fe/harvard.jpg")] 
+      h-150 bg-cover bg-center 
+      w-full bg-max-w-screen-xl mt-10 px-8 xl:px-16 mx-auto'
+    >
+     {/* <div className='
+         bg-gradient-to-tl h-auto from-gray-900  w-full relative'
+      > */}
+      
+      {/* <img src='https://projects.iq.harvard.edu/files/styles/os_files_xxlarge/public/testcivicengagement/files/48875402378_d10761b291_o.jpg_01.jpg' 
+            className='w-full h-full object-cover absolute mix-blend-overlay' />
+       */}
 
-        </div>
-        <div className="flex w-full">
-          <div className="h-full w-full">
-            {/* <Image
-              src="/assets/Illustration1.png"
-              alt="VPN Illustrasi"
-              quality={100}
-              width={612}
-              height={383}
-              layout="responsive"
-            /> */}
+      <div className='px-20 py-2'>
 
-        <div className="rounded-lg w-full grid grid-flow-row grid-cols-1 py-9 divide-y-2 divide-x-0 divide-gray-100 bg-white-500 z-10">
+        {/* <div className="bg-scroll bg-my_bg_image h-[972px] max-w-screen-xl mt-10 px-8 xl:px-16 mx-auto" id="about"> */}
           
-            <h1 className="text-3xl text-black-600 font-bold py-6 px-4">
-                Key Dates
+        <div className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16 ">
+          <div className="inline-block align-top py-20 flex-col justify-center items-start row-start-2 sm:row-start-1">
+            <h1 className="text-5xl xl:text-6xl font-medium text-black-600 leading-normal">
+              Our future is on the <strong>ballot</strong>.
             </h1>
-            
-            {listUser.map((listUsers, index) => (
-                <div
-                className="flex items-right justify-right py-6 px-4 w-auto mx-0"
-                key={index}
-                >
-                <div className="flex mr-auto w-auto">
-                    <div className="flex items-center justify-center bg-orange-100 w-12 h-12 mr-6 rounded-full">
-                    <img src={listUsers.icon} className="h-6 w-6" />
-                    </div>
-                    <div className="flex flex-col">
-                    <p className="text-xl text-black-600 font-bold">
-                        {listUsers.number}
-                    </p>
-                    <p className="text-lg text-black-500">{listUsers.name}</p>
-                    </div>
-                </div>
-                </div>
-            ))}
-
-              <button
-                className="text-xl font-bold text-orange-500 hover:text-orange-600 bg-white transition-all duration-300  text-medium p-3 py-6 px-5 sm:px-8 "
-              >
-                See full events calendar
-              </button>
-
-            </div>
+            <p className="text-black-500 mt-4 mb-6">
+              Vote in the Massachusetts primaries on September 6th.
+            </p>
+            {/* <ButtonPrimary>Get Started</ButtonPrimary> */}
+              <Button >Meet the Candidates</Button>
 
           </div>
+
+          
+
+          <div className="flex w-full">
+            <div className="h-full w-full">
+              {/* <Image
+                src="/assets/Illustration1.png"
+                alt="VPN Illustrasi"
+                quality={100}
+                width={612}
+                height={383}
+                layout="responsive"
+              /> */}
+
+          <div className="rounded-lg w-full grid grid-flow-row grid-cols-1 py-9 divide-y-2 divide-x-0 divide-gray-100 bg-white-500 z-10">
+            
+              <h1 className="text-3xl text-black-600 font-bold py-6 px-4">
+                  Key Dates
+              </h1>
+              
+              {listUser.map((listUsers, index) => (
+                  <div
+                  className="flex items-right justify-right py-6 px-4 w-auto mx-0"
+                  key={index}
+                  >
+                  <div className="flex mr-auto w-auto">
+                      <div className="flex items-center justify-center bg-orange-100 w-12 h-12 mr-6 rounded-full">
+                      <img src={listUsers.icon} className="h-6 w-6" />
+                      </div>
+                      <div className="flex flex-col">
+                      <p className="text-xl text-black-600 font-bold">
+                          {listUsers.number}
+                      </p>
+                      <p className="text-lg text-black-500">{listUsers.name}</p>
+                      </div>
+                  </div>
+                  </div>
+              ))}
+
+                <button
+                  className="text-xl font-bold text-orange-500 hover:text-orange-600 bg-white transition-all duration-300  text-medium p-3 py-6 px-5 sm:px-8 "
+                >
+                  See full events calendar
+                </button>
+
+              </div>
+
+            </div>
+          </div>
         </div>
+
       </div>
+
+      
+
       <div className="relative w-full flex">
         {/* <div className="rounded-lg w-full grid grid-flow-row sm:grid-flow-row grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-gray-100 bg-white-500 z-10">
           {listUser.map((listUsers, index) => (
@@ -128,6 +150,8 @@ const Hero = ({
           style={{ filter: "blur(114px)" }}
         ></div>
       </div>
+
+      
     </div>
   );
 };
